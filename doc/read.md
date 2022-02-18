83081a13b871f557622f1d20efe7aff822ccf87a
ziggle <muyue1125@gmail.com>
2022-02-19 03:46:10 +0800

    init

---
+++ doc/read.md
@@ -0,0 +1,11 @@
+# 生成私钥
+
+`openssl genrsa -out app_private_key.pem   2048 `
+
+# Java开发者需要将私钥转换成PKCS8格式
+
+`openssl pkcs8 -topk8 -inform PEM -in app_private_key.pem -outform PEM -nocrypt -out app_private_key_pkcs8.pem`
+
+# 生成公钥
+
+`openssl rsa -in app_private_key.pem -pubout -out app_public_key.pem`
--- src/index.php
+++ src/index.php
@@ -3,18 +3,11 @@ require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
-
 $client = new Client();
-$response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+$response = $client->request('GET',
+    'https://api.github.com/repos/guzzle/guzzle', ['verify' => false]);
 
-echo $response->getStatusCode(); // 200
-echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
-echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+echo $response->getStatusCode() . PHP_EOL; // 200
+echo $response->getHeaderLine('content-type') . PHP_EOL; // 'application/json; charset=utf8'
+echo $response->getBody() . PHP_EOL; // '{"id": 1420053, "name": "guzzle", ...}'
 
-// Send an asynchronous request.
-$request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
-$promise = $client->sendAsync($request)->then(function ($response) {
-    echo 'I completed! ' . $response->getBody();
-});
-
-$promise->wait();
